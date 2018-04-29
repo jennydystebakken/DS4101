912236f1130a2372ae02bb7e406b33449a8cee7a
Jenny Mylius <jkm.sandmo@gmail.com>
2018-04-29 21:32:33 +0200

    Reaktor ferdig :-)

--- Mappeinnlevering/scripts/apps/ReactorApp.js
+++ Mappeinnlevering/scripts/apps/ReactorApp.js
@@ -42,16 +42,17 @@ const ReactorApp = {
                 })
             $(".reactor-temp-v")
                 .css({
-                    width: "70px",
-                    height: "650px",
-                    border: "solid"
+                    height: "50px",
+                    width: "650px",
+                    border: "solid",
+                    margin: "1rem"
                 })
             $(".reactor-temp-h")
                 .css({
-                    width: "70px",
-                    height: "650px",
+                    height: "50px",
+                    width: "650px",
                     border: "solid",
-                    marginLeft: "3rem"
+                    margin: "1rem"
                 })
            $("#Temperatur") 
                 .css({
@@ -138,9 +139,7 @@ const ReactorApp = {
                 .text('Skru opp temperaturen')
                 .on('click', function () {
                     /*Skriver hva knappen gjør*/
-                    RA.arrowShakingHSlow()
-                    RA.arrowShakingHFast()
-                    RA.rotatePumpV()
+                    
 
                 })
 
@@ -148,9 +147,7 @@ const ReactorApp = {
                 .text('Skru ned temperaturen')
                 .on('click', function () {
                     /*Skriver hva knappen gjør*/
-                    RA.arrowShakingHSlow()
-                    RA.arrowShakingHFast()
-                    RA.rotatePumpV()
+                    
 
                 })
 
@@ -158,11 +155,79 @@ const ReactorApp = {
                 .text('Slå av reaktorer')
                 .on('click', function () {
                     /*Skriver hva knappen gjør*/
-                    RA.arrowShakingHSlow()
-                    RA.arrowShakingHFast()
-                    RA.rotatePumpV()
+                    
 
                 })
+                
+                $("#slider1").slider(
+                    {
+                        min: 50,
+                        max: 700,
+                        step: 100,
+                        //value: 50,
+                        values: [50],
+                        orientation: "horizontal",
+                        slide: function(event, ui){
+                            console.log(ui.value); 
+                            console.log(ui.values[0] + " " + ui.values[1]); 
+                            
+                            setBoxSize(ui.value);
+                        }
+                    }
+                );
+                
+                function setBoxSize(size){
+                    $("#reactor-v")
+                        .css(
+                            {
+                                width: size
+                            }
+                        )
+                }
+                
+                let box = $("#reactor-v")
+                    .css(
+                        {
+                            width: 70,
+                            height: 50,
+                            backgroundColor: "green"
+                        }
+                    );
+
+                    $("#slider2").slider(
+                        {
+                            min: 50,
+                            max: 700,
+                            step: 100,
+                            //value: 50,
+                            values: [50],
+                            orientation: "horizontal",
+                            slide: function(event, ui){
+                                console.log(ui.value); 
+                                console.log(ui.values[0] + " " + ui.values[1]); 
+                                
+                                setBoxSize2(ui.value);
+                            }
+                        }
+                    );
+                    
+                    function setBoxSize2(size){
+                        $("#reactor-h")
+                            .css(
+                                {
+                                    width: size
+                                }
+                            )
+                    }
+                    
+                    let box2 = $("#reactor-h")
+                        .css(
+                            {
+                                width: 70,
+                                height: 50,
+                                backgroundColor: "green"
+                            }
+                        );
                 
 
             div.append(btn1, btn2, btn3, btn4, btn5)
@@ -235,4 +300,6 @@ const ReactorApp = {
         })
         
     },
+
+    
 }
